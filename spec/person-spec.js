ddfec716f70e961e8e613a51fed8db398b3d4510
Elly Maimon <eliranmaimon@gmail.com>
2018-08-03 12:43:46 -0700

    Rewrite tests again to fix date conversion issue

--- spec/person-spec.js
+++ spec/person-spec.js
@@ -3,12 +3,10 @@ import { Person } from '../src/person.js';
 describe('Person', function () {
     let reusablePerson;
     let reusableDate;
-    let reusableCurrenTime;
 
     beforeEach(function () {
         reusablePerson = new Person(26, 1991, 8, 21);
-        reusableDate = new Date("1991-08-21");
-        reusableCurrenTime = new Date();
+        reusableDate = new Date("08/21/1991");
     });
 
     it('should test whether a Person has an age, a birth date, and a current date', function () {
@@ -16,7 +14,7 @@ describe('Person', function () {
         expect(reusablePerson.birthDate.getFullYear()).toEqual(1991);
         expect(reusablePerson.birthDate.getMonth()).toEqual(7);
         expect(reusablePerson.birthDate.getDate()).toEqual(21);
-        expect(reusablePerson.currentDate.getMonth()).toEqual(reusableCurrenTime.getMonth());
+        expect(reusablePerson.currentDate.getMonth()).toEqual(7);
     });
 
     it('should test whether a the age of a person can be converted to seconds', function () {
@@ -26,11 +24,11 @@ describe('Person', function () {
     });
 
     it('should test whether the age of the user in seconds can be calculated', function() {
-        let ageInSeconds =  reusablePerson.ageInSeconds();
+        let ageInSeconds = reusablePerson.ageInSeconds();
         let x = reusablePerson.currentDate.getTime() / 1000; //seconds from 1/1/1970 to current date (8/3/2018)
         let y = reusableDate.getTime() / 1000; //seconds from 1/1/1970 to 8/21/1991
         let expectedAgeInSeconds = x - y;
-        
+        console.log(ageInSeconds);
         expect(ageInSeconds).toEqual(expectedAgeInSeconds);
     });
 });
