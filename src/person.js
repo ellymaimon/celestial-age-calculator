ef1ef0317fa3709ae79526c9cb63549a0b21ceec
Elly Maimon <eliranmaimon@gmail.com>
2018-08-03 12:44:06 -0700

    Add function to calculate a user's age in seconds, pass spec

--- src/person.js
+++ src/person.js
@@ -1,7 +1,7 @@
 export class Person {
   constructor(ageInYears, birthYear, birthMonth, birthDay) {
     this.ageInYears = ageInYears;
-    this.birthDate = new Date(birthYear + "-" + birthMonth + "-" + birthDay);
+    this.birthDate = new Date("0" + birthMonth + "/" + birthDay + "/" + birthYear);
     this.currentDate = new Date();
   };
 
@@ -10,7 +10,14 @@ export class Person {
     return ageInSeconds;
   }
 
-  
+  ageInSeconds() {
+    let currentTime = this.currentDate.getTime() / 1000;
+    let birthTime = this.birthDate.getTime() / 1000;
+    let ageInSeconds = currentTime - birthTime;
+    return ageInSeconds;
+  }
+
+
 }
 
 //day = 86400 seconds
