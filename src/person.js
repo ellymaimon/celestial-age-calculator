3f749deae843ac538b7b396c555d2b73880e9495
Elly Maimon <eliranmaimon@gmail.com>
2018-08-03 16:15:11 -0700

    Fix function, add case handling for when user outlives expectancy

--- src/person.js
+++ src/person.js
@@ -18,7 +18,7 @@ export class Person {
     if (this.birthDate.getTime() < 0) {
       let currentTime = this.currentDate.getTime() / 1000;
       let birthTime = (-this.birthDate.getTime() + this.currentDate.getTime()) / 1000;
-      let ageInSeconds = currentTime - birthTime;
+      let ageInSeconds = currentTime + birthTime;
       return ageInSeconds;
     } 
     else {
@@ -90,57 +90,72 @@ export class Person {
       let expectancy = this.lifeExpectancy / this.planetOrbitalPeriods[0];
       let userAgeInPlanet = this.ageInPlanetYears(planet);
       
-      // if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
-      // else 
-      return expectancy - userAgeInPlanet;
+      if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
+      else return expectancy - userAgeInPlanet;
     }
 
     if (planet === "venus") {
       let expectancy = this.lifeExpectancy / this.planetOrbitalPeriods[1];
       let userAgeInPlanet = this.ageInPlanetYears(planet);
-      return expectancy - userAgeInPlanet;
+
+      if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
+      else return expectancy - userAgeInPlanet;
     }
 
     if (planet === "earth") {
       let expectancy = this.lifeExpectancy / this.planetOrbitalPeriods[2];
       let userAgeInPlanet = this.ageInPlanetYears(planet);
-      return expectancy - userAgeInPlanet;
+
+      if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
+      else return expectancy - userAgeInPlanet;
     }
 
     if (planet === "mars") {
       let expectancy = this.lifeExpectancy / this.planetOrbitalPeriods[3];
       let userAgeInPlanet = this.ageInPlanetYears(planet);
-      return expectancy - userAgeInPlanet;
+
+      if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
+      else return expectancy - userAgeInPlanet;
     }
 
     if (planet === "jupiter") {
       let expectancy = this.lifeExpectancy / this.planetOrbitalPeriods[4];
       let userAgeInPlanet = this.ageInPlanetYears(planet);
-      return expectancy - userAgeInPlanet;
+
+      if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
+      else return expectancy - userAgeInPlanet;
     }
 
     if (planet === "saturn") {
       let expectancy = this.lifeExpectancy / this.planetOrbitalPeriods[5];
       let userAgeInPlanet = this.ageInPlanetYears(planet);
-      return expectancy - userAgeInPlanet;
+
+      if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
+      else return expectancy - userAgeInPlanet;
     }
 
     if (planet === "uranus") {
       let expectancy = this.lifeExpectancy / this.planetOrbitalPeriods[6];
       let userAgeInPlanet = this.ageInPlanetYears(planet);
-      return expectancy - userAgeInPlanet;
+
+      if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
+      else return expectancy - userAgeInPlanet;
     }
 
     if (planet === "neptune") {
       let expectancy = this.lifeExpectancy / this.planetOrbitalPeriods[7];
       let userAgeInPlanet = this.ageInPlanetYears(planet);
-      return expectancy - userAgeInPlanet;
+
+      if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
+      else return expectancy - userAgeInPlanet;
     }
 
     if (planet === "pluto") {
       let expectancy = this.lifeExpectancy / this.planetOrbitalPeriods[8];
       let userAgeInPlanet = this.ageInPlanetYears(planet);
-      return expectancy - userAgeInPlanet;
+
+      if(userAgeInPlanet > expectancy) return userAgeInPlanet - expectancy;
+      else return expectancy - userAgeInPlanet;
     }
   }
 }
