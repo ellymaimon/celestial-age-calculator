96ec3b1024eed828a82a941b5ae94ed51fb78aa6
Elly Maimon <eliranmaimon@gmail.com>
2018-08-03 13:48:40 -0700

    Add failing specs for remaining planets

--- spec/person-spec.js
+++ spec/person-spec.js
@@ -21,7 +21,7 @@ describe('Person', function () {
         expect(ageInSeconds).toEqual(expectedAgeInSeconds);
     });
 
-    it('should test whether the age of the user in seconds can be calculated', function() {
+    it('should test whether the age of the user in seconds can be calculated', function () {
         let ageInSeconds = reusablePerson.ageInSeconds();
 
         let secondsToCurrentDate = reusablePerson.currentDate.getTime() / 1000; //seconds from 1/1/1970 to current date (8/3/2018)
@@ -31,60 +31,102 @@ describe('Person', function () {
         expect(ageInSeconds).toEqual(expectedAgeInSeconds);
     });
 
-    it('should test whether the age of the user in Mercury years can be calculated', function() {
+    it('should test whether the age of the user in Mercury years can be calculated', function () {
         let planet = "mercury";
         let ageInMercury = reusablePerson.ageInPlanetYears(planet);
 
         let ageInSeconds = reusablePerson.ageInSeconds();
         let secondsInMercuryYear = reusablePerson.secondsInYear * 0.241;
-        let expectedAgeInMercury = ageInSeconds/secondsInMercuryYear;
+        let expectedAgeInMercury = ageInSeconds / secondsInMercuryYear;
 
         expect(ageInMercury).toEqual(expectedAgeInMercury);
     });
 
-    it('should test whether the age of the user in Venus years can be calculated', function() {
+    it('should test whether the age of the user in Venus years can be calculated', function () {
         let planet = "venus";
         let ageInVenus = reusablePerson.ageInPlanetYears(planet);
 
         let ageInSeconds = reusablePerson.ageInSeconds();
         let secondsInVenusYear = reusablePerson.secondsInYear * 0.6152;
-        let expectedAgeInVenus = ageInSeconds/secondsInVenusYear;
+        let expectedAgeInVenus = ageInSeconds / secondsInVenusYear;
 
         expect(ageInVenus).toEqual(expectedAgeInVenus);
     });
 
-    it('should test whether the age of the user in Earth years can be calculated', function() {
+    it('should test whether the age of the user in Earth years can be calculated', function () {
         let planet = "earth";
         let ageInEarth = reusablePerson.ageInPlanetYears(planet);
 
         let ageInSeconds = reusablePerson.ageInSeconds();
         let secondsInEarthYear = reusablePerson.secondsInYear;
-        let expectedAgeInEarth = ageInSeconds/secondsInEarthYear;
+        let expectedAgeInEarth = ageInSeconds / secondsInEarthYear;
 
         expect(ageInEarth).toEqual(expectedAgeInEarth);
     });
 
-    it('should test whether the age of the user in Mars years can be calculated', function() {
+    it('should test whether the age of the user in Mars years can be calculated', function () {
         let planet = "mars";
         let ageInMars = reusablePerson.ageInPlanetYears(planet);
 
         let ageInSeconds = reusablePerson.ageInSeconds();
         let secondsInMarsYear = reusablePerson.secondsInYear * 1.8809;
-        let expectedAgeInMars = ageInSeconds/secondsInMarsYear;
+        let expectedAgeInMars = ageInSeconds / secondsInMarsYear;
 
         expect(ageInMars).toEqual(expectedAgeInMars);
     });
 
-    it('should test whether the age of the user in Jupiter years can be calculated', function() {
+    it('should test whether the age of the user in Jupiter years can be calculated', function () {
         let planet = "jupiter";
         let ageInJupiter = reusablePerson.ageInPlanetYears(planet);
 
         let ageInSeconds = reusablePerson.ageInSeconds();
         let secondsInJupiterYear = reusablePerson.secondsInYear * 11.8618;
-        let expectedAgeInJupiter = ageInSeconds/secondsInJupiterYear;
+        let expectedAgeInJupiter = ageInSeconds / secondsInJupiterYear;
 
         expect(ageInJupiter).toEqual(expectedAgeInJupiter);
     });
 
+    it('should test whether the age of the user in Saturn years can be calculated', function () {
+        let planet = "saturn";
+        let ageInSaturn = reusablePerson.ageInPlanetYears(planet);
 
+        let ageInSeconds = reusablePerson.ageInSeconds();
+        let secondsInSaturnYear = reusablePerson.secondsInYear * 29.457;
+        let expectedAgeInSaturn = ageInSeconds / secondsInSaturnYear;
+
+        expect(ageInSaturn).toEqual(expectedAgeInSaturn);
+    });
+
+    it('should test whether the age of the user in Uranus years can be calculated', function () {
+        let planet = "uranus";
+        let ageInUranus = reusablePerson.ageInPlanetYears(planet);
+
+        let ageInSeconds = reusablePerson.ageInSeconds();
+        let secondsInUranusYear = reusablePerson.secondsInYear * 84;
+        let expectedAgeInUranus = ageInSeconds / secondsInUranusYear;
+
+        expect(ageInUranus).toEqual(expectedAgeInUranus);
+    });
+
+    it('should test whether the age of the user in Neptune years can be calculated', function () {
+        let planet = "neptune";
+        let ageInNeptune = reusablePerson.ageInPlanetYears(planet);
+
+        let ageInSeconds = reusablePerson.ageInSeconds();
+        let secondsInNeptuneYear = reusablePerson.secondsInYear * 164.8;
+        let expectedAgeInNeptune = ageInSeconds / secondsInNeptuneYear;
+
+        expect(ageInNeptune).toEqual(expectedAgeInNeptune);
+    });
+
+    it('should test whether the age of the user in Pluto years can be calculated', function () {
+        let planet = "pluto";
+        let ageInPluto = reusablePerson.ageInPlanetYears(planet);
+
+        let ageInSeconds = reusablePerson.ageInSeconds();
+        let secondsInPlutoYear = reusablePerson.secondsInYear * 248;
+        let expectedAgeInPluto = ageInSeconds / secondsInPlutoYear;
+
+        expect(ageInPluto).toEqual(expectedAgeInPluto);
+    });
 });
