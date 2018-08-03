98dc22514ec14e992bac758cfbf72b2f599657c3
Elly Maimon <eliranmaimon@gmail.com>
2018-08-03 12:05:27 -0700

    Update spec to new standard, pass specs

--- spec/person-spec.js
+++ spec/person-spec.js
@@ -4,19 +4,26 @@ describe('Person', function () {
     let reusablePerson;
 
     beforeEach(function () {
-        reusablePerson = new Person(26, 1991, 8, 21);
+        reusablePerson = new Person(26, 1991, 08, 21);
     });
 
-    it('should test whether a Person has an age, birth year, birth month, and birth day', function () {
+    it('should test whether a Person has an age, a birth date, and a current date', function () {
         expect(reusablePerson.ageInYears).toEqual(26);
-        expect(reusablePerson.birthYear).toEqual(1991);
-        expect(reusablePerson.birthMonth).toEqual(8);
-        expect(reusablePerson.birthDay).toEqual(21);
+        expect(reusablePerson.birthDate.getFullYear()).toEqual(1991);
+        expect(reusablePerson.birthDate.getMonth()).toEqual(7);
+        expect(reusablePerson.birthDate.getDate()).toEqual(21);
+        expect(reusablePerson.currentDate.getMonth()).toEqual(new Date().getMonth());
     });
 
     it('should test whether a the age of a person can be converted to seconds', function () {
-        let ageInSeconds = reusablePerson.convertToSeconds();
+        let ageInSeconds = reusablePerson.convertYearsToSeconds();
         let expected = 26 * 31557600;
         expect(ageInSeconds).toEqual(expected);
     });
+
+    // it('should test whether the age of the user in seconds can be calculated', function() {
+    //     let ageInSeconds =  reusablePerson.ageInSeconds();
+    //     let expected = (26 * 31557600) + (86400 * ;
+    //     expect(ageInSeconds).toEqual(expected);
+    // });
 });
