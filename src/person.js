d7d8c44e2fe8d56eede1bd6259dd1748c7c57da0
Elly Maimon <eliranmaimon@gmail.com>
2018-08-03 13:25:33 -0700

    Add function to determine age in Venus years, prep for modularization

--- src/person.js
+++ src/person.js
@@ -4,6 +4,8 @@ export class Person {
     this.birthDate = new Date("0" + birthMonth + "/" + birthDay + "/" + birthYear);
     this.currentDate = new Date();
     this.secondsInYear = 31557600;
+    //In order: Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto
+    this.planetOrbitalPeriods = [0.241, 0.6152, 1.8809, 11.8618, 29.457, 84, 164.8, 248];
   };
 
 
@@ -22,7 +24,21 @@ export class Person {
 
   ageInMercuryYears() {
     let ageInSeconds = this.ageInSeconds();
-    let secondsInMercuryYear = this.secondsInYear * 0.24;
+    let secondsInMercuryYear = this.secondsInYear * 0.241;
     return ageInSeconds/secondsInMercuryYear;
   }
+
+  ageInVenusYears() {
+    let ageInSeconds = this.ageInSeconds();
+    let secondsInVenusYear = this.secondsInYear * 0.6152;
+    return ageInSeconds/secondsInVenusYear;
+  }
+
+  // ageInPlanetYears(planet) {
+  //   if (planet == "mercury") {
+  //     let ageInSeconds = this.ageInSeconds();
+  //     let secondsInPlanetYear = this.secondsInYear * this.planetOrbitalPeriods[0];
+  //     return ageInSeconds/secondsInPlanetYear; 
+  //   }
+  // }
 }
