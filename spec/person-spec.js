89639a5ec07756b17889207df70875c52dffcc05
Elly Maimon <eliranmaimon@gmail.com>
2018-08-03 10:59:52 -0700

    Add spec for converting to seconds

--- spec/person-spec.js
+++ spec/person-spec.js
@@ -13,4 +13,10 @@ describe('Person', function () {
         expect(reusablePerson.birthMonth).toEqual(8);
         expect(reusablePerson.birthDay).toEqual(21);
     });
+
+    it('should test whether a the age of a person can be converted to seconds', function () {
+        let ageInSeconds = reusablePerson.convertToSeconds();
+        let expected = 26 * 31557600;
+        expect(ageInSeconds).toEqual(expected);
+    });
 });
